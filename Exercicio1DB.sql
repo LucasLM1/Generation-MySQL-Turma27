126733088c12fd6b741539957e610e421eb9eb13
LucasLM1 <lucascorreia325@gmail.com>
2021-07-20 22:44:31 -0300

    MySQL

---
+++ Exercicio1DB.sql
@@ -0,0 +1,23 @@
+/*create database db_servrh;*/
+
+/*use db_servrh;*/
+
+/*Create table tb_funcionarios(
+	id bigint (10) auto_increment,
+	nome varchar (20) not null,
+    salario decimal (4,2) not null,
+    cargo varchar (15),
+    setor decimal (2)
+);*/
+
+/*insert into tb_funcionarios (nome, salario, cargo, setor) Values ("Lucas", 4100, "Supervisor", "01");
+insert into tb_funcionarios (nome, salario, cargo, setor) Values ("James", 3700, "Desenvolvedor", "05");
+insert into tb_funcionarios (nome, salario, cargo, setor) Values ("Flavia", 1100, "Assistente", "04");
+insert into tb_funcionarios (nome, salario, cargo, setor) Values ("Karina", 1200, "Estagiaria", "02");
+insert into tb_funcionarios (nome, salario, cargo, setor) Values ("Rodrigo", 2800, "Scrum Master", "03");*/
+
+/*select * from tb_funcionarios where salario > 2000;
+select * from tb_funcionarios where salario < 2000;*/
+
+/*update tb_funcionarios set cargo = "Coordenador" where id = 1;
+
